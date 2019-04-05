1ae676d8bc634305be3c39beeb35ca5f40d23cf2
juanpp94 <12-10566@usb.ve>
2019-04-05 01:01:49 -0400

    Update cdad3.sh
    Descarga la tabla original desde la pagina y la renombra como pruebaMediana

--- cdad3.sh
+++ cdad3.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/juanpp94/Proyecto1-Bases-de-Datos1/master/pruebaMediana.csv
+wget https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.4.zip
+unzip simplemaps_worldcities_basicv1.4.zip
+rm simplemaps_worldcities_basicv1.4.zip  worldcities.xlsx license.txt 
+mv -v worldcities.csv pruebaMediana.csv
+
 psql -f cdad3.sql
